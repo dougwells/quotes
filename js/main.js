d1b9ad3236bf6ee86478edd9695da6cff9c6dfca
dougwells <dougwells@gmail.com>
2016-01-18 14:53:35 -0700

    Caption over photo.  Lots of work ... now understand fundamentals

--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
-$(window).scroll(function(){
-    if($('.navbar').offset().top > 50){
-        $('.navbar-fixed-top').addClass('top-nav-collapse');
-    } else {
-        $('.navbar-fixed-top').removeClass('top-nav-collapse');
-    }
-    $('#navbar li a').blur();
-});
+
 
 
 
 $(document).ready(function () {
+
+  //Download new quote & image URL from json datafile stored in my dropbox
+  //Replaces existing quote and image on webpage
+  $("button").click(function(){
+    $.ajax({
+      url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
+      dataType: 'json',
+      success: function(data) {
+        var i = Math.floor(Math.random() * (data.length));
+        var steveQuote = data[i].quote;
+        var imageUrl = data[i].imageUrl;
+        $("em").text("'"+steveQuote+"'");
+        $('img').attr("src", imageUrl)
+        console.log(steveQuote);
+        console.log(imageUrl);
+        console.log(i);
+      },
+      error: function(err) {
+        var steveQuote = "This app is shit!  You have an error. Error Message: "+ err;
+        $("em").text(steveQuote);
+        console.log(steveQuote);
+      }
+    });
+  });
+
   //hide hamburger menu once clicked ...
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
-  });
-
-  //Gives HTML page a "scrolling" effect when click on link in SPA.  "Scrolls" down.
-  //Must include following line in HTML file for following jquery code to work
-  //<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-  $(function(){
-      $('.page-scroll a').bind('click', function(){
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500,'easeInOutExpo');
-          event.preventDefault();
-      });
   });
 });
