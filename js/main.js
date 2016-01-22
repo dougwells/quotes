6b5e5449a72d9099a7fcf39bebad3fc952c15bb6
dougwells <dougwells@gmail.com>
2016-01-22 07:34:57 -0700

    Buttons working again.  Z layout conflict now.  Ugh

--- js/main.js
+++ js/main.js
@@ -2,29 +2,25 @@ $(document).ready(function() {
 
   //Download new quote & image URL from json datafile stored in my dropbox
   //Replaces existing quote and image on webpage
-  // $(".newQuote").click(function(){
-  //   $.ajax({
-  //     url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
-  //     dataType: 'json',
-  //     success: function(data) {
-  //       var i = Math.floor(Math.random() * (data.length));
-  //       var imageUrl = data[i].imageUrl;
-  //       var author = data[i].author;
-  //       var steveQuote = data[i].quote;
-  //
-  //       $(".quote").text("'"+steveQuote+"'");
-  //       $(".author").text("'"+steveQuote+"'");
-  //       $('.quote-image').attr("src", imageUrl)
-  //       console.log(steveQuote);
-  //       console.log(imageUrl);
-  //       console.log(i);
-  //     },
-  //     error: function(err) {
-  //       var steveQuote = "This app is shit!  You have an error. Error Message: "+ err;
-  //       $(".quote").text(steveQuote);
-  //       $(".author").text("Steve Jobs");
-  //       console.log(steveQuote);
-  //     }
-  //   });
-  // });
+  $("button").click(function(){
+    $.ajax({
+      url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
+      dataType: 'json',
+      success: function(data) {
+        var i = Math.floor(Math.random() * (data.length));
+        var steveQuote = data[i].quote;
+        var imageUrl = data[i].imageUrl;
+        $("em").text("'"+steveQuote+"'");
+        $('img').attr("src", imageUrl)
+        console.log(steveQuote);
+        console.log(imageUrl);
+        console.log(i);
+      },
+      error: function(err) {
+        var steveQuote = "This app is shit!  You have an error. Error Message: "+ err;
+        $("em").text(steveQuote);
+        console.log(steveQuote);
+      }
+    });
+  });
 });
