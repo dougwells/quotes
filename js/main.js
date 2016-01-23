70ed05c90e50830246939b4a2f3bfc67bef21264
dougwells <dougwells@gmail.com>
2016-01-23 06:28:55 -0700

    All functionality & layout working. Next couple commits for polish

--- js/main.js
+++ js/main.js
@@ -2,23 +2,30 @@ $(document).ready(function() {
 
   //Download new quote & image URL from json datafile stored in my dropbox
   //Replaces existing quote and image on webpage
-  $("button").click(function(){
+  $("#newQuote").click(function(){
     $.ajax({
       url: 'https://dl.dropboxusercontent.com/s/p5zx9nv7qpyexnc/quotes.json',
       dataType: 'json',
       success: function(data) {
         var i = Math.floor(Math.random() * (data.length));
         var steveQuote = data[i].quote;
+        var author = data[i].author;
         var imageUrl = data[i].imageUrl;
-        $("em").text("'"+steveQuote+"'");
-        $('img').attr("src", imageUrl)
+        var imageColor = data[i].imageColor;
+        $(".quote").text("'"+steveQuote+"'");
+        $('.quote-image').attr("src", imageUrl);
+        $('.author').text('Author: '+author);
         console.log(steveQuote);
         console.log(imageUrl);
-        console.log(i);
+        console.log(imageColor);
       },
       error: function(err) {
         var steveQuote = "This app is shit!  You have an error. Error Message: "+ err;
-        $("em").text(steveQuote);
+        var author = "Steve Jobs"
+        var imageUrl = "https://dl.dropboxusercontent.com/s/5ris4qilrskw5mr/sucks.jpg"
+        $(".quote").text("'"+steveQuote+"'");
+        $('.quote-image').attr("src", imageUrl);
+        $('.author').text('Author: '+author);
         console.log(steveQuote);
       }
     });
