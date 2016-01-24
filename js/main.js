ca97cc5206daa66355991737959d9bb53a6ba102
dougwells <dougwells@gmail.com>
2016-01-23 20:09:42 -0700

    "Share on Twitter" DONE.  Fun challenge.

--- js/main.js
+++ js/main.js
@@ -8,25 +8,31 @@ $(document).ready(function() {
       dataType: 'json',
       success: function(data) {
         var i = Math.floor(Math.random() * (data.length));
-        var steveQuote = data[i].quote;
+        var quote = data[i].quote;
         var author = data[i].author;
         var imageUrl = data[i].imageUrl;
         var imageColor = data[i].imageColor;
-        $(".quote").text("'"+steveQuote+"'");
-        $('.quote-image').attr("src", imageUrl);
-        $('.author').text('Author: '+author);
-        console.log(steveQuote);
-        console.log(imageUrl);
-        console.log(imageColor);
+        var twitterPost = "Love this quote by "+author+": '"+quote+"'";
+        var twitterPost140 = twitterPost.length > 140 ? twitterPost.substring(0,137)+"..." : twitterPost;
+        var quoteURI = encodeURI(twitterPost140);
+        var twitterQueryString = "https://twitter.com/intent/tweet?text="+quoteURI;
+        $('#quote').text("'"+quote+"'");
+        $('#quote-image').attr("src", imageUrl);
+        $('#author').text('Author: '+author);
+        $("#shareTwitter").attr("href", twitterQueryString)
+        console.log(twitterPost);
+        console.log(twitterPost.length);
+        console.log(twitterPost140);
+        console.log(twitterPost140.length);
       },
       error: function(err) {
-        var steveQuote = "This app is shit!  You have an error. Error Message: "+ err;
+        var quote = "This app is shit!  You have an error. Error Message: "+ err;
         var author = "Steve Jobs"
         var imageUrl = "https://dl.dropboxusercontent.com/s/5ris4qilrskw5mr/sucks.jpg"
-        $(".quote").text("'"+steveQuote+"'");
-        $('.quote-image').attr("src", imageUrl);
-        $('.author').text('Author: '+author);
-        console.log(steveQuote);
+        $("#quote").text("'"+quote+"'");
+        $('#quote-image').attr("src", imageUrl);
+        $('#author').text('Author: '+author);
+        console.log(quote);
       }
     });
   });
